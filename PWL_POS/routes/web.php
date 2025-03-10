94a5dbe31cc56c3aa485c9ce3b7aa3d944730171
Tomi Martino Affandi <tomimartino10@gmail.com>
2025-03-10 18:15:51 +0700

    implementasi query builder

--- PWL_POS/routes/web.php
+++ PWL_POS/routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\levelController;
 
-Route::get('/', function(){
+Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/level', [levelController::class, 'index']);
+Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
