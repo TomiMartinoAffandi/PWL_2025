10f9d2637bcaa36fe2219c45036eab7d0e3a57fd
Tomi Martino Affandi <tomimartino10@gmail.com>
2025-03-10 19:09:18 +0700

    Implementasi Eloquent ORM

--- PWL_POS/resources/views/user.blade.php
+++ PWL_POS/resources/views/user.blade.php
@@ -1,7 +1,25 @@
-<h1>Profil Pengguna</h1>
-<p>ID: {{ $id }}</p>
-<p>Nama: {{ $name }}</p>
-<a href="{{ url('/') }}" class="mt-4 inline-block bg-gray-500 text-white px-4 py-2 rounded-lg shadow-md hover:bg-gray-600">
-    Kembali ke Home
-</a>
-
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Data User</title>
+</head>
+<body>
+    <h1>Data User</h1>
+    <table border="1" cellpadding="2" cellspacing="0">
+        <tr>
+            <th>ID</th>
+            <th>Username</th>
+            <th>Nama</th>
+            <th>ID Level Pengguna</th>
+        </tr>
+        @foreach ($data as $d)
+        <tr>
+            <td>{{ $d->user_id }}</td>
+            <td>{{ $d->username }}</td>
+            <td>{{ $d->nama }}</td>
+            <td>{{ $d->level_id }}</td>
+        </tr>
+        @endforeach
+    </table>
+</body>
+</html>
