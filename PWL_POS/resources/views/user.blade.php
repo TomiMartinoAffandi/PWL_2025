dde3ab6b0587f06d56b9856cb8ca618349a1b470
Tomi Martino Affandi <tomimartino10@gmail.com>
2025-03-15 21:33:02 +0700

    retrieving or creating models

--- PWL_POS/resources/views/user.blade.php
+++ PWL_POS/resources/views/user.blade.php
@@ -1,9 +1,23 @@
-<h1>Data User</h1>
-<table border="1">
-    <tr>
-        <th>Jumlah Pengguna</th>
-    </tr>
-    <tr>
-        <td>{{ $data }}</td>
-    </tr>
-</table>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Data User</title>
+</head>
+<body>
+    <h1>Data User</h1>
+    <table border="1" cellpadding="2" cellspacing="0">
+        <tr>
+            <th>ID</th>
+            <th>Username</th>
+            <th>Nama</th>
+            <th>ID Level Pengguna</th>
+        </tr>
+        <tr>
+            <td>{{ $data->user_id }}</td>
+            <td>{{ $data->username }}</td>
+            <td>{{ $data->nama }}</td>
+            <td>{{ $data->level_id }}</td>
+        </tr>
+    </table>
+</body>
+</html>
